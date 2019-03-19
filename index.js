750b3512bf9fd45bbb1efb5778aea263fd6cb550
Alexander Remie <user@rmi7.me>
2019-03-19 15:46:07 +0100

    fixes/improvements

--- index.js
+++ index.js
@@ -26,21 +26,32 @@ const json = require(thePath);
 
 let res = `contract ${json.contractName} {\n`;
 
-const contractNode = json.ast.nodes.find(item => item.name === json.contractName);
+const findLastFnDefinition = (fnName) => {
+	for (let i = json.ast.nodes.length - 1; i >= 0; i -= 1) {
+		const outerNode = json.ast.nodes[i];
+		if (outerNode.nodeType !== 'ContractDefinition') continue;
+		for (let j = 0; j < outerNode.nodes.length; j += 1) {
+			const innerNode = outerNode.nodes[j];
+			if (innerNode.nodeType !== 'FunctionDefinition' && innerNode.nodeType !== 'VariableDeclaration') continue;
+			if (innerNode.name === fnName) return innerNode;
+		}
+	}
+}
+
 
 json.abi.forEach((fn) => {
-  if (fn.type === 'constructor' || fn.type === 'event') return;
-	const fnNode = contractNode.nodes.find(item => item.nodeType === 'FunctionDefinition' && item.name === fn.name);
-
+	if (fn.type === 'constructor' || fn.type === 'event') return;
+	const fnNode = findLastFnDefinition(fn.name);
+	// if (fn.name === 'symbol') console.log({ fnNode });
   res += '    function ';
-  if (fn.type === 'function') res += fn.name;
+  if (fn.type === 'function') res += fn.name; // otherwise it's fallback function
   res += '(';
 
   if (fn.inputs && fn.inputs.length) {
     const args = fn.inputs.map((inp, idx) => {
-			const paramNode = fnNode && fnNode.parameters.parameters[idx];
+			const paramNode = fnNode && fnNode.nodeType === 'FunctionDefinition' && fnNode.parameters.parameters[idx];
 			let inpStr = inp.type;
-			if (fnNode && paramNode.storageLocation !== 'default') inpStr += ` ${paramNode.storageLocation}`;
+			if (fnNode && fnNode.nodeType === 'FunctionDefinition' && paramNode.storageLocation !== 'default') inpStr += ` ${paramNode.storageLocation}`;
       if (inp.name) inpStr += ` ${inp.name}`;
 			return inpStr;
     }).join(', ');
@@ -49,17 +60,16 @@ json.abi.forEach((fn) => {
   }
   res += ') ';
 
-  if (fn.stateMutability && fn.stateMutability != 'nonpayable') res += `${fn.stateMutability} `
-
-	res += 'external';
-
+  if (fn.stateMutability && fn.stateMutability != 'nonpayable') res += `${fn.stateMutability} `;
+	res += (!fnNode || fnNode.nodeType === 'VariableDeclaration') ? 'public' : fnNode.visibility;
 	if (!fn.outputs || !fn.outputs.length) res += ';\n';
   else {
     res += ' returns('
     const out_args = fn.outputs.map((outp, idx) => {
-			const paramNode = fnNode && fnNode.returnParameters.parameters[idx];
+			const paramNode = fnNode && fnNode.nodeType === 'FunctionDefinition' && fnNode.returnParameters.parameters[idx];
 			let outpStr = outp.type;
-			if (fnNode && paramNode.storageLocation !== 'default') outpStr += ` ${paramNode.storageLocation}`;
+			if (fnNode && fnNode.nodeType === 'FunctionDefinition' && paramNode.storageLocation !== 'default') outpStr += ` ${paramNode.storageLocation}`;
+			else if (outp.type === 'bytes' || outp.type === 'string' || outp.type.includes('[]')) outpStr += ' memory';
 			if (outp.name) outpStr += ` ${outp.name}`;
 			return outpStr;
     }).join(', ');
